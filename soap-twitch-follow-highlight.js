4eef7c575d6c6381cb3306868165fd41ca11141e
Technosoap <technosoap.git@null.local>
2025-04-28 15:01:08 +0100

    Userscript sign of life

---
+++ soap-twitch-follow-highlight.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         soap-twitch-follow-highlight
+// @namespace    http://tampermonkey.net/
+// @version      2025-04-28
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.twitch.tv/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log(`hello everybody`);
+    document.body.style.padding = "32px";
+})();
