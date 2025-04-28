3577b259aca1374029ea85b2f3d72dc0b60f80f0
Technosoap <technosoap.git@null.local>
2025-04-28 15:37:26 +0100

    Managed to change text colour, yay

--- soap-twitch-follow-highlight.js
+++ soap-twitch-follow-highlight.js
@@ -14,4 +14,13 @@
 
     console.log(`hello everybody`);
     document.body.style.padding = "32px";
+
+    const changeColor = function() {
+        //document.getElementsByClassName("side-nav-card__title")[1].firstElementChild.style.backgroundColor = "#ffff00";
+        //document.getElementsByClassName("side-nav-card__title")[1].firstElementChild.style.color = "#ff0000 !important";
+        document.getElementsByClassName("side-nav-card__title")[1].firstElementChild.setAttribute( 'style', 'color: #ff0000 !important' );
+    };
+
+    setInterval(changeColor, 1000);
+
 })();
