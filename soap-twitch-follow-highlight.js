2fa8ec8ec64423343e794a02a235d8058f62ccf4
Technosoap <technosoap.git@null.local>
2025-04-30 14:53:36 +0100

    Fix the FrankerFaceZ issue by switching to `classList.toggle`

--- soap-twitch-follow-highlight.js
+++ soap-twitch-follow-highlight.js
@@ -6,21 +6,20 @@
 // @author       Technosoap
 // @match        https://www.twitch.tv/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
-
-// TODO breaks the FrankerFaceZ extension "Hide Offline Channels" feature :(
 
 (function() {
     'use strict';
 
+    GM_addStyle('.soap-twitch-follow-highlight-gray { color: gray !important; }');
+    GM_addStyle('.soap-twitch-follow-highlight-lime { color: lime !important; }');
+
     // The long term plan is that this JSON would be stored in local storage and modified by some sort of configuration interface. For now, we hard-code it.
     // assumption: trusted input
     //
-    // TODO nest the highlights one layer deeper, within an object with one-key
-    // value pair called "usersToHighlight". Generally nice to have the
-    // top-level use only keys under our control.
-    const highlightsJson = '{"GiantBombForever": "#888888", "OldTimeyComputerShow": "#888888"}';
+    // TODO validate that colours here have a corresponding soap-twitch-follow-highlight-* class.
+    const userConfigJson = '{"highlights": {"GiantBombForever": "gray", "OldTimeyComputerShow": "gray", "WonderlandRogue": "lime"}}';
 
     // Revive with Object.create(null), to avoid issues of a namespace collision
     // between Object's prototype and twitch users (imagine a twitch streamer
@@ -29,7 +28,7 @@
     // ref:
     // https://esdiscuss.org/topic/proposal-add-an-option-to-omit-prototype-of-objects-created-by-json-parse#content-1
     // via https://stackoverflow.com/a/48260948
-    const highlights = JSON.parse(highlightsJson, function(key, value) {
+    const userConfig = JSON.parse(userConfigJson, function(key, value) {
         if (value && typeof value === 'object' && !Array.isArray(value)) {
             return Object.assign(Object.create(null), value);
         }
@@ -39,9 +38,9 @@
     const changeColor = function() {
         for (const titleElem of document.getElementsByClassName("side-nav-card__title")) {
             const handle = titleElem.firstElementChild.title;
-            const highlightColor = highlights[handle];
+            const highlightColor = userConfig.highlights[handle];
             if (highlightColor) {
-                titleElem.firstElementChild.setAttribute('style', `color: ${highlightColor} !important`);
+                titleElem.firstElementChild.classList.toggle(`soap-twitch-follow-highlight-${highlightColor}`, true);
             }
         }
     };
